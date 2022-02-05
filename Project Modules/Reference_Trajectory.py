5ef85ca3a35031aa6afb55d2810661703f811405
Baraghini Nicholas <baraghini.nicholas@gmail.com>
2022-02-05 11:43:48 +0100

    task 3 and 1 last updates

--- Project Modules/Reference_Trajectory.py
+++ Project Modules/Reference_Trajectory.py
@@ -1,60 +1,6 @@
 from scipy.interpolate import CubicSpline
 import numpy as np
 import matplotlib.pyplot as plt
-def Cubic_Ref(time,params,TT,dim,plot = 0):
-
-    t1= np.arange(0, time[0], params['dt'])
-    Tf1 = time[0]
-    L = params['LL']
-    nx = dim[0]
-    nu = dim[1]
-    a2 = 9 / 8 * L
-    a3 = -3 / 4 * L
-    cycle1 = a2 * (t1 ** 2) + a3 * (t1 ** 3)
-    cycle1_vel = 2 * t1 * a2 + 3 * a3 * (t1 ** 2)
-
-    Tf2 = time[1]
-    t2 = np.arange(Tf1, Tf2, params['dt'])
-    a0_2 = 3 / 8 * L
-    a2_2 = -(21 / 8 * L) / ((Tf2 - Tf1) ** 2)
-    a3_2 = (14 / 8 * L) / ((Tf2 - Tf1) ** 3)
-    cycle2 = a0_2 + a2_2 * ((t2 - Tf1) ** 2) + a3_2 * ((t2 - Tf1) ** 3)
-    cycle2_vel = 2 * (t2 - Tf1) * a2_2 + 3 * a3_2 * ((t2 - Tf1) ** 2)
-
-    Tf3 = time[2]
-    t3 = np.arange(Tf2, Tf3, params['dt'])
-    a0_3 = -L / 2;
-    a2_3 = (3 / 2 * L) / ((Tf3 - Tf2) ** 2);
-    a3_3 = (- L) / ((Tf3 - Tf2) ** 3)
-    cycle3 = a0_3 + a2_3 * ((t3 - Tf2) ** 2) + a3_3 * ((t3 - Tf2) ** 3)
-    cycle3_vel = 2 * (t3 - Tf2) * a2_3 + 3 * a3_3 * ((t3 - Tf2) ** 2)
-
-    tot_time = time[2]
-    tt = np.arange(0, tot_time, params['dt'])
-    traj = np.concatenate((cycle1.T, cycle2.T, cycle3.T))
-    traj_vel = np.concatenate((cycle1_vel.T, cycle2_vel.T, cycle3_vel.T))
-
-    if plot:
-        fig, ax = plt.subplots(figsize=(20, 10))
-        ax.plot(tt, traj, 'o-', label='pos')
-        ax.plot(tt, traj_vel, '.-', label='vel')
-        ax.legend(loc='lower left', ncol=2)
-        plt.grid()
-        plt.show()
-
-    xx_ref = np.zeros((dim[0], TT))
-    xx_ref[0, 0:TT] = traj
-    xx_ref[1, 0:TT] = traj_vel
-
-    # input reference definition
-    uu_ref = np.zeros((dim[1], TT))
-    uu_ref[0, 0:TT] = params['mm']*params['gg'] * xx_ref[0,:]
-
-    out = {
-        'xx_ref': xx_ref,
-        'uu_ref': uu_ref
-    }
-    return out
 
 def Spline_Ref(x_point_ref, t_point_ref, TT, params, plot=0):
     #INPUTS:
@@ -108,6 +54,7 @@ def Spline_Ref(x_point_ref, t_point_ref, TT, params, plot=0):
     uu_ref = np.zeros((nu, TT))
     uu_ref_qs = params['mm'] * params['gg'] * xx_ref_pos        # input torque in qs condition
     uu_ref[0, 0:TT] = uu_ref_qs                                 #reference inputs samples in qs condition
+
 
     out = {
         'xx_ref':xx_ref,
